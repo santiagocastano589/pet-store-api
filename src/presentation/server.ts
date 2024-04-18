0988189834d33147a8a3ae58fdc4f3f352b777e0
juanjose <juanjoperez2121@gmail.com>
2024-04-18 13:26:44 -0500

    Creacion de Rutas de la Api

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,26 @@
-import  express  from "express";
+import  express, { Router }  from "express"; //2
 
-// creamos una interfaz para configurar el parametro de port
 interface Options{
     port?: number;
+    routes: Router; //1
 }
 
 export class Server {   
     
     public readonly app = express();
     private readonly port: number;
+    private readonly routes: Router; //4
    
     constructor( options: Options ) {
-        const { port = 3100 } = options;
+        const { port = 3100, routes } = options; //3
         this.port = port;
+        this.routes = routes; //5
     }
         
    async start() {
+
+        this.app.use(this.routes); //6
+
         this.app.listen(this.port, () => {
             console.log(`Server is running on port ${this.port}`);
         });
