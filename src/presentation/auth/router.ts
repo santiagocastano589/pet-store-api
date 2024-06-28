3a4aaf77b8c27e94c764665dc24bf10e5e6eccb7
Santiago Castaño <santiagocastano589@gmail.com>
2024-06-28 16:42:23 -0500

    documentacion de la api y endpoints

--- src/presentation/auth/router.ts
+++ src/presentation/auth/router.ts
@@ -6,19 +6,43 @@ export class AuthRoutes {
 
 
     static get routes(): Router {
-  
+
       const router = Router();
 
       const datasource = new AuthDataSourceImpl(); //6
       const AuthRepository = new AuthRepositoryImpl(datasource); //7
       const controller = new AuthController(AuthRepository); // 2 // 8 actulizacion
-  
+
         // Definir todas mis rutas especificas
-        router.post('/login', controller.loginUser) // 3
+
+
+router.post('/login', controller.loginUser) // 3
+
         router.post('/register', controller.registerUser) // 4
-  
-  
-        return router;
-    }
-  
+/**
+ * @openapi
+ * /api/auth/register:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: Registro de Usuarios
+ *     description: |
+ *       Este es un endPoint para registrar usuarios.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/userSchema'
+ *     responses:
+ *       '200':
+ *         description: Retorna el objeto de la colección.
+ *       '422':
+ *         description: Error con los datos.
+ */
+
+
+      return router;
   }
+
+}
--- src/presentation/router.ts
+++ src/presentation/router.ts
@@ -1,19 +1,26 @@
+
 import { Router } from 'express'; // 2
 import { AuthRoutes } from './auth/router'; //6
+import swaggerUi from 'swagger-ui-express'
+import swaggerSetup from '../docs/swagger'
+
+
 
 export class AppRoutes { // 1
 
 
     static get routes(): Router { //3
-  
+
       const router = Router(); //4
-  
+
       // Definir todas mis rutas principales
+      router.use('/documentacion',swaggerUi.serve,swaggerUi.setup(swaggerSetup))
+
       router.use('/api/auth', AuthRoutes.routes ) // 7
 
  
       return router; //5
     }
-  
-  
-}
+
+
+ }
